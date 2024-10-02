0942265d79d82c18dfb864c3f7f4c9158f60f6a7
TheGeneral00 <richard.beldner@outlook.de>
2024-10-02 18:59:44 +0200

    added full functionality to existing functions, added debugging function printResponse to print json string of an http request

--- main.go
+++ main.go
@@ -5,20 +5,26 @@ import (
     "bufio";
     "os";
     "net/http";
-    "encoding/json"
+    "encoding/json";
+    "io"
 )
 
 
 type cliCommand struct {
     name string
     description string 
-    callback func() error
+    callback func(*config) error
+}
+
+type config struct {
+    prev string
+    next string
 }
 
 type Response struct {
 	Count    int    `json:"count"`
 	Next     string `json:"next"`
-	Previous any    `json:"previous"`
+	Previous string    `json:"previous"`
 	Results  []struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
@@ -38,7 +44,7 @@ func getCommands() map[string]cliCommand {
             callback:       commandExit,
         },
         "map": {
-            name:           "map",
+            name:           "map ",
             description:    "Displays the name of 20 locations",
             callback:       commandMap,
         },
@@ -47,16 +53,21 @@ func getCommands() map[string]cliCommand {
             description:    "Displays the locations from the previous map call",
             callback:       commandMapB,
         },
+        "printResponse": {
+            name:           "printResponse",
+            description:    "Debugging function that prints the response of a http request",
+            callback:       printResponse,
+        },
     }
 }
 
-func commandExit() error {
+func commandExit(config *config) error {
     fmt.Println("Exiting program")
     os.Exit(0)
     return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *config) error {
     commands := getCommands()
     fmt.Println("Welcome to the Pokedex!\n\nUsage:\n")
     for name, content := range commands {
@@ -66,8 +77,11 @@ func commandHelp() error {
     return nil
 }
 
-func commandMap() error {
-    res, err := http.Get("https://pokeapi.co/api/v2/location")
+func commandMap(config *config) error {
+    res, err := http.Get("https://pokeapi.co/api/v2/location-area")
+    if config.next != ""{
+        res, err = http.Get(config.next)
+    }
     if err != nil {
         return fmt.Errorf("Locations couldn't be displayed with error: %v", err)
     }
@@ -83,27 +97,74 @@ func commandMap() error {
     for _, location := range response.Results {
         fmt.Println(location.Name)
     }
+    config.next = response.Next
+    config.prev = response.Previous
     return nil
 }
 
-func commandMapB() error {
-    fmt.Println("To be implemented")
-    return nil
+func commandMapB(config *config) error {
+    if config.prev == "" {
+        return fmt.Errorf("There are no locations to go back to")
+    }
+    res, err := http.Get(config.prev)
+    if err != nil {
+       return fmt.Errorf("Locations couldn't be displayed with error: %v", err)
+    }
+    defer res.Body.Close()
+    if res.StatusCode > 299 {
+        return fmt.Errorf("Response failed with status code: %d", res.StatusCode)
+    }
+    var response Response
+    dec := json.NewDecoder(res.Body)
+    if err := dec.Decode(&response); err != nil {
+        return fmt.Errorf("Failed with error: %v", err)
+    }
+    for _, location := range response.Results {
+        fmt.Println(location.Name)
+    }
+    config.next = response.Next
+    config.prev = response.Previous
+    return nil 
 }
 
+func printResponse(config *config) error {
+    res, err := http.Get("https://pokeapi.co/api/v2/location-area")
+    if err != nil {
+        return fmt.Errorf("Response failed with error: %v", err)
+    }
+    if res.StatusCode > 299 {
+        return fmt.Errorf("Response failed with status code: %v\nand body: \n", res.StatusCode, res.Body)
+    }
+    defer res.Body.Close()
+    bodyBytes, err := io.ReadAll(res.Body)
+    if err != nil {
+        return fmt.Errorf("Reading of response body failed")
+    }
+    fmt.Printf("%s\n", string(bodyBytes))
+    return nil
+}
 
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
+    var config config
     commands := getCommands()
     for {
         fmt.Printf("pokedex > ")
         if scanner.Scan() {
-            _, ok := commands[scanner.Text()]
-            if !ok {
-                fmt.Println("Invalid command")
-                continue
-            } 
-            commands[scanner.Text()].callback()
+            switch scanner.Text() {
+            case "help":
+                commands["help"].callback(&config)
+            case "exit":
+                commands["exit"].callback(&config)
+            case "map":
+                commands["map"].callback(&config)
+            case "mapb":
+                commands["mapb"].callback(&config)
+            case "printResponse":
+                commands["printResponse"].callback(&config)
+            default:
+                fmt.Printf("%v is not a valid command", scanner.Text())
+            }    
         }
          
     }
